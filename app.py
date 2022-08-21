98a1f6988b399bc4253474dbcd9d09f5ee81ffe8
Soura10 <souradeeproy.10@gmail.com>
2022-08-21 22:28:52 +0530

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CICD Pipeline has been established"
 
 
 if __name__=="__main__":
